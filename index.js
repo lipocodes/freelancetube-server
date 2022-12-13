a6894df20c5e2ab49c4eacff742e3af26d17c639
Lior <lipocodes@yahoo.com>
2022-12-13 14:27:05 +0200

    6th commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
-import path from "path";
 
 const connect = ()=> {
  mongoose.connect(process.env.MONGO).then(()=>console.log("Connected to MongoDB!")).catch((err)=> {throw err});   
@@ -9,15 +8,7 @@ const connect = ()=> {
 
 const app = express();
 dotenv.config();
-
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static("/client/build"));
-    app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
-   });
- }
-
-app.listen(process.env.PORT||8800, ()=>{
+app.listen("8800", ()=>{
     connect();
     console.log("Server is running!")
 });
